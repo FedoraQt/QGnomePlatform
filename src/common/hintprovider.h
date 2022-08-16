082e2d1c6a07407931477afb312a21ce2bdcce8c
Jan Grulich <jgrulich@redhat.com>
2022-08-16 13:04:57 +0200

    Update copyrights

--- src/common/hintprovider.h
+++ src/common/hintprovider.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2022 Jan Grulich
+ * Copyright (C) 2016-2022 Jan Grulich <jgrulich@redhat.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
